ba55e6481f5af4f33e7315c19f9ef6d555aea3b3
Syed Ahsan Ahmed <snahmed1998@gmail.com>
2018-06-15 22:37:50 +0500

    added modules

---
+++ app.js
@@ -0,0 +1,29 @@
+const electron = require('electron')
+const url = require('url')
+const path = require('path')
+
+const {app, BrowserWindow} = electron;
+
+let mainWindow;
+
+
+app.on('ready', function () {
+	mainWindow = new BrowserWindow({})
+	mainWindow.loadURL(url.format({
+		pathname: path.join(__dirname, 'pages/home.html'),
+		protocol: 'file:',
+		slashes: true
+	}));
+});
+
+
+/**
+
+mainWindow = new BrowserWindow({});
+mainWindow.loadURL(url.format({
+	pathname: path.join(__dirname, 'pages/home.html'),
+	protocol: 'file:',
+	slashes: true
+}))
+
+**/
