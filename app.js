7b883b8e9820da9239270b818d8d0de881283afd
Syed Ahsan Ahmed <snahmed1998@gmail.com>
2018-06-17 18:55:15 +0500

    ipc working

--- app.js
+++ app.js
@@ -23,6 +23,7 @@ app.on('ready', function () {
 
 
     const { ipcMain } = require('electron')
+
     ipcMain.on('asynchronous-message', (event, arg) => {
         console.log(arg) // prints "ping"
         event.sender.send('asynchronous-reply', 'pong')
