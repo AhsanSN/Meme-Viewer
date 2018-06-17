17622b879c3c26013f90ec258749e1e289804c94
Syed Ahsan Ahmed <snahmed1998@gmail.com>
2018-06-17 11:11:55 +0500

    window showing

--- app.js
+++ app.js
@@ -1,30 +1,10 @@
 const electron = require('electron')
-
-const { app, BrowserWindow } = require('electron')
-
-function createWindow() {
-    // Create the browser window.
-    win = new BrowserWindow({ width: 800, height: 600 })
-
-    // and load the index.html of the app.
-    win.loadFile('index.html')
-}
-
-app.on('ready', createWindow)
-
-
-
-/**
-
-const electron = require('electron')
 const url = require('url')
 const path = require('path')
 
 const {app, BrowserWindow} = electron;
 
 let mainWindow;
-
-
 
 app.on('ready', function () {
 	mainWindow = new BrowserWindow({})
@@ -36,35 +16,3 @@ app.on('ready', function () {
 });
 
 
-
-
-
-const { browserwindow } = require('electron')
-let win = new browserwindow({ width: 400, height: 320 })
-
-win.on('close', () => { win = null })
-win.loadurl(modalpath)
-win.show()
-
-const newwindowbtn = document.getelementbyid('new-window')
-
-newWindowBtn.addEventListener('click', (event) => {
-    const modalPath = path.join('file://', __dirname, '../../sections/windows/modal.html')
-    let win = new BrowserWindow({ width: 400, height: 320 })
-
-    win.on('close', () => { win = null })
-    win.loadURL(modalPath)
-    win.show()
-})
-**/
-
-
-
-/**
-mainWindow = new BrowserWindow({});
-mainWindow.loadURL(url.format({
-	pathname: path.join(__dirname, 'pages/home.html'),
-	protocol: 'file:',
-	slashes: true
-}))
-**/
