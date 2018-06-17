1c086633ea8693f3cc1fdc6b4ecc406cad52687c
Syed Ahsan Ahmed <snahmed1998@gmail.com>
2018-06-17 18:26:01 +0500

    updated modules

--- app.js
+++ app.js
@@ -2,13 +2,12 @@ const electron = require('electron')
 const url = require('url')
 const path = require('path')
 
-const {app, BrowserWindow} = electron;
+const { app, BrowserWindow, ipcMain } = electron;
 
 let mainWindow;
 
 
 function launchingMainWindow() {
-    mainWindow = new BrowserWindow({})
     mainWindow.setMenu(null);
     mainWindow.setTitle("Meme Viewer");
     mainWindow.setSize(800, 600, 1);
@@ -17,11 +16,15 @@ function launchingMainWindow() {
         protocol: 'file:',
         slashes: true
     }));
+    mainWindow.webContents.send('newImg',"asd")
+
 }
 
 app.on('ready', function () {
+    mainWindow = new BrowserWindow({})
     launchingMainWindow();
     console.log("1");
+
 });
 
 
