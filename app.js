91afc3b45878173d2573b86643a771c600b2ba87
Syed Ahsan Ahmed <snahmed1998@gmail.com>
2018-06-18 12:19:47 +0500

    tested with images. about added

--- app.js
+++ app.js
@@ -3,53 +3,34 @@ const url = require('url')
 const path = require('path')
 const fs = require('fs');
 
-
 const { app, BrowserWindow } = electron;
-
 let mainWindow;
 
-function writeToFile(data="no text provided") {
-
+function writeToFile(data = "no text provided") {
+    data = data + '\n';
     fs.appendFile('imagesFile.txt', data, function (err, data) {
         if (err) console.log(err);
-        console.log("Successfully Written to File.");
+        console.log("Address added");
     });
 }
 
-function readFile() {
+function readFile(callback) {
     var arrayImg;
-    fs.readFile('imagesFile.txt', function (err, data) {
-        if (err) throw err;
-        arrayImg = data.toString().split("\n");
-      
-        console.log(arrayImg)
-        return arrayImg;
-    });
-    return arrayImg;
-    /**
-     * for (i in array) {
-            console.log("t1"+array[i]);
+    fs.readFile('imagesFile.txt', function (error, data) {
+        if (error)
+            callback(error, null);
+        else {
+            arrayImg = data.toString().split("\n");
+            callback(null, arrayImg);
         }
-
-     * **/
+    });
 }
 
-function ipc() {
+function ipc(imgArray) {
     var imgNumber = 0;
-    var imgArray = [];
-    imgArray.push("../images/m1.png");
-    imgArray.push("../images/m (1).png");
-    imgArray.push("../images/m (2).png");
-    imgArray.push("../images/m (3).png");
-    imgArray.push("../images/m (4).png");
-    imgArray.push("../images/m (5).png");
-    imgArray.push("../images/m (6).png");
-
     const { ipcMain } = require('electron')
     ipcMain.on('asynchronous-message', (event, arg) => {
         console.log(arg) // prints "ping"
-        var d = new Date();
-        var n = d.getTime();
 
         event.sender.send('asynchronous-reply', imgArray[imgNumber])
         if (imgNumber+1 == imgArray.length) {
@@ -76,14 +57,14 @@ function showWindow() {
     //writeToFile();
 }
 
-
 app.on('ready', function () {
     showWindow();
-    //ipc();
-    var imgArray = readFile();
-    console.log(imgArray);
-
-
+    //getting images array
+    var imgArray = readFile(function (err, data) {
+        if (err) {
+            console.log("ERROR : ", err);
+        } else {
+            ipc(data);
+        }
+    });
 });
-
-
