2ce4d56e4cf4d926c376857d30c4c0cfeaf23288
Syed Ahsan Ahmed <snahmed1998@gmail.com>
2018-06-18 09:20:21 +0500

    addr changing and sending

--- app.js
+++ app.js
@@ -6,6 +6,28 @@ const { app, BrowserWindow } = electron;
 
 let mainWindow;
 
+function ipc() {
+    var imgNumber = 0;
+    var imgArray = [];
+    imgArray.push("/1");
+    imgArray.push("/2");
+    imgArray.push("/3");
+    const { ipcMain } = require('electron')
+    ipcMain.on('asynchronous-message', (event, arg) => {
+        console.log(arg) // prints "ping"
+        var d = new Date();
+        var n = d.getTime();
+
+        event.sender.send('asynchronous-reply', imgArray[imgNumber])
+        if (imgNumber+1 == imgArray.length) {
+            imgNumber = 0;
+        }
+        else {
+            imgNumber++;
+        }
+        event.returnValue = 'pong'
+    })
+}
 
 function showWindow() {
     mainWindow = new BrowserWindow({})
@@ -17,17 +39,7 @@ function showWindow() {
         protocol: 'file:',
         slashes: true
     }));
-
-    const { ipcMain } = require('electron')
-    ipcMain.on('asynchronous-message', (event, arg) => {
-        console.log(arg) // prints "ping"
-        var d = new Date();
-        var n = d.getTime();
-        event.sender.send('asynchronous-reply', n)
-        event.returnValue = 'pong'
-    })
-
-
+    ipc();
 }
 
 
