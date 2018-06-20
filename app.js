73cf6de4a8e1cebf22a410adc3003ea04b08f799
Syed Ahsan Ahmed <snahmed1998@gmail.com>
2018-06-20 23:22:07 +0500

    formatted

--- app.js
+++ app.js
@@ -12,7 +12,9 @@ let mainWindow;
 var nImages = 0;
 var googleIndex = 1;
 
-function getImgFromArray(dataArray) {
+function getImgFromArray(dataArray)
+{
+
     console.log(dataArray.items.length);
     var imgArrayDownloaded = []
     for (var i = 0; i < dataArray.items.length; i++) {
@@ -22,13 +24,18 @@ function getImgFromArray(dataArray) {
     return imgArrayDownloaded;
 }
 
-function downloadGoogleArray(googleArray, arrayImg) {
+function downloadGoogleArray(googleArray, arrayImg)
+{
+
     for (let i = 0; i < googleArray.length; i++) {
         downloadImg(googleArray[i], arrayImg);
     }
+
 }
 
-function downloadImg(url = "https://www.google.com/images/srpr/logo3w.png", arrayImg) {
+function downloadImg(url = "https://www.google.com/images/srpr/logo3w.png", arrayImg)
+{
+
     var download = function (uri, filename, callback) {
         request.head(uri, function (err, res, body) {
             nImages = nImages + 1;
@@ -37,27 +44,37 @@ function downloadImg(url = "https://www.google.com/images/srpr/logo3w.png", arra
             console.log('content-length:', res.headers['content-length']);
             console.log('added ' + filename);
             writeToFile("." + filename);
+
             nImages = nImages + 1;
             arrayImg.push("." + filename);
+
             request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);
         });
     };
+
     download(url, "random", function () {
     });
+
 }
 
-function getImgfromNet(arrayImg) {
+function getImgfromNet(arrayImg)
+{
+
     if (module === require.main) {
         const options = {
             q: "memes",
             apiKey: "AIzaSyAwUpzM9DJr58Y3y_8TMnMkfwCBtCEGcTs",
             cx: "010789280150233095101:gry9brqojdc",
         };
+
         imgRequest(options, arrayImg).catch(console.error);
     }
+
 }
 
-async function imgRequest(options, arrayImg) {
+async function imgRequest(options, arrayImg)
+{
+
     const res = await customsearch.cse.list({
         cx: options.cx,
         q: options.q,
@@ -65,19 +82,26 @@ async function imgRequest(options, arrayImg) {
         searchType: "image",
         start: googleIndex
     });
+
     var googleArray = getImgFromArray(res.data);
     googleIndex = googleIndex + 10;
     downloadGoogleArray(googleArray, arrayImg);
     //console.log(res.data.items[0].link);
     return res.data;
+
 }
 
-function writeToFile(data = "no text provided") {
+function writeToFile(data = "no text provided")
+{
+
     stream.write(data + '\n');
     console.log("Address added");
+
 }
 
-function readFile(callback) {
+function readFile(callback)
+{
+
     var arrayImg;
     fs.readFile('imagesFile.txt', function (error, data) {
         if (error)
@@ -88,15 +112,20 @@ function readFile(callback) {
             callback(null, arrayImg);
         }
     });
+
 }
 
-function ipc(imgArray) {
+function ipc(imgArray)
+{
+
     var imgNumber = 0;
+
     const { ipcMain } = require('electron')
     ipcMain.on('asynchronous-message', (event, arg) => {
        // console.log(arg) // prints "ping"
         getImgfromNet(imgArray);
         console.log("-------------------------------------------------------" + googleIndex)
+
         event.sender.send('asynchronous-reply', imgArray[imgNumber])
         if (imgNumber + 1 == imgArray.length) {
             imgNumber = 0;
@@ -106,26 +135,34 @@ function ipc(imgArray) {
         }
         event.returnValue = 'pong'
     })
+
 }
 
 function showWindow() {
+
     mainWindow = new BrowserWindow({})
     mainWindow.setMenu(null);
     mainWindow.setTitle("Meme Viewer");
     mainWindow.setSize(800, 600, 1);
     mainWindow.setResizable(false)
+
     mainWindow.loadURL(url.format({
         pathname: path.join(__dirname, '/pages/home.html'),
         protocol: 'file:',
         slashes: true
     }));
+
     //writeToFile();
 }
 
 //main
+
 app.on('ready', function () {
+
     showWindow();
+
     var imgArray = readFile(function (err, data) {
+
         if (err) {
             console.log("ERROR : ", err);
         } else {
@@ -133,5 +170,7 @@ app.on('ready', function () {
             getImgfromNet(data);
             ipc(data);
         }
+
     });  
+
 });
